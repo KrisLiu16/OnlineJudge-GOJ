3d6520e2e0cf6129d27c7048105ec6f52894ff58
Kris <luter.liu@foxmail.com>
2025-10-28 15:59:44 +0800

    Update stats.go

--- goj-backend/pkg/controllers/stats.go
+++ goj-backend/pkg/controllers/stats.go
@@ -2,8 +2,8 @@ package controllers
 
 import (
 	"context"
-	"goj/pkg/config"
-	"goj/pkg/models"
+	"github.com/KrisLiu16/OnlineJudge-GOJ/goj-backend/pkg/config"
+	"github.com/KrisLiu16/OnlineJudge-GOJ/goj-backend/pkg/models"
 	"runtime"
 	"strconv"
 	"strings"
