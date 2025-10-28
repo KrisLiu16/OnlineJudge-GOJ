e1907ccd3f92594b3c531d118216e16c1773779d
Kris <luter.liu@foxmail.com>
2025-10-28 16:03:44 +0800

    Update judge.go

--- goj-backend/pkg/judge/judge.go
+++ goj-backend/pkg/judge/judge.go
@@ -1,8 +1,8 @@
 package judge
 
 import (
-	"goj/pkg/judge/manager"
-	"goj/pkg/judge/types"
+	"github.com/KrisLiu16/OnlineJudge-GOJ/goj-backend/pkg/judge/manager"
+	"github.com/KrisLiu16/OnlineJudge-GOJ/goj-backend/pkg/judge/types"
 )
 
 // Submit 提交评测任务
